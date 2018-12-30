6ae28711d87a2acd1979e462ee4b587d13f63f5c
Kacpersky <kacper.mekarski@gmail.com>
2018-12-30 12:07:22 +0100

    before_action at controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,13 @@
 class ArticlesController < ApplicationController
 
+  before_action :find_params, only: [:edit, :show, :destroy, :update]
+
   def index
     @articles = Article.all
   end
 
   def new
     @article = Article.new
-  end
-
-  def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -24,18 +22,12 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    flash[:notice] = "Article was successfully deleted."
-    redirect_to articles_path
+  def edit
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -44,6 +36,16 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted."
+    redirect_to articles_path
+  end
+
+  private
+  def find_params
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
