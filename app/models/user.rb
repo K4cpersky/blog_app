8b586d0e4dd9b297855bbf6d96456bf438f662bd
Kacpersky <kacper.mekarski@gmail.com>
2019-01-02 09:54:09 +0100

    Edit update users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: {maximum: 105 },
             uniqueness: { case_sensitive: false },
             format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true
   has_secure_password
+
 end
