e585947cf33a50ee41ed0e1201d1e28d0f7954e4
Kacpersky <kacper.mekarski@gmail.com>
2019-01-05 19:16:12 +0100

    Update all views with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,20 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @category_articles = @category.articles.paginate(page:params[:page], per_page: 5)
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name was succesfully updated"
+      redirect_to category_path(category)
+    else
   end
 
   private
